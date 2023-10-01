8d91aa6d046a9eb6bd35494789ce50ad12c4f8af
Quinten Bruce <qcbruce7@gmail.com>
2023-10-01 17:13:35 -0500

    edit main_test.py

--- main_test.py
+++ main_test.py
@@ -5,14 +5,23 @@ import orjson
 
 #test 3 different sizes of input
 def test_main_S():
+    f = open("tests/output/t_outputS.json", "r")
+    output = f.read()
+    x = orjson.loads(output)
+    print(f"\n\n\n", '{:-^50}'.format('small output file'))
+    print("\noutput file: \n", output)
+
     main.main("tests/output/t_outputS.json")
     f = open("tests/records/t_recordsS.json", "r")
     output = f.read()
-    print(output)
+    
     x = orjson.loads(output)
     f = open("records.json", "r")
     output = f.read()
     y = orjson.loads(output)
+    print("records file produced: \n", output)
+    if x == y:
+        print("test passed!")
     assert(x == y)
 
 def test_main_M():
@@ -23,6 +32,10 @@ def test_main_M():
     f = open("records.json", "r")
     output = f.read()
     y = orjson.loads(output)
+    print(f"\n\n", '{:-^50}'.format('medium output file'))
+    if x == y:
+        print("test passed!")
+
     assert(x == y)
 
 def test_main_L():
@@ -33,4 +46,7 @@ def test_main_L():
     f = open("records.json", "r")
     output = f.read()
     y = orjson.loads(output)
+    print(f"\n\n", '{:-^50}'.format('large output file'))
+    if x == y:
+        print("test passed!")
     assert(x == y)
