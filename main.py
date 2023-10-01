2241b89fa332d75ec72847a3bc3354e5f14aa6ba
Quinten Bruce <qcbruce7@gmail.com>
2023-10-01 15:42:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import orjson
+import os
+
+def main(fileName: str):
+    f = open(fileName, "r")
+    output = f.read()
+    data = orjson.loads(output)
+    coll = {}  # key:id, value:index
+    records = []
+    for i in range(len(data)):  # O(n) time complexity
+        if data[i]["id"] in coll:
+            if data[coll[data[i]["id"]]]["type"] == "START":
+                start = data[coll[data[i]["id"]]]
+                end = data[i]
+            else:
+                start = data[i]
+                end = data[coll[data[i]["id"]]]
+            record = {
+                "sessionId": start["id"],
+                "sessionStartTime": start["timestamp"],
+                "sessionEndTime": end["timestamp"],
+                "sessionDuration": int(end["timestamp"]) - int(start["timestamp"]),
+                "returnedLate": True if int(end["timestamp"]) - int(start["timestamp"]) > 86400 else False,
+                "damaged": True if end["comments"] != "" else False
+            }
+            records.append(record)
+            del coll[data[i]["id"]]
+        else:
+            coll[data[i]["id"]] = i
+
+    d = orjson.dumps(records)
+    with open("records.json", "wb") as f:
+        f.write(d)
+
+if __name__ == "__main__":
+    main()
---
+++ main_test.py
@@ -0,0 +1,44 @@
+import main
+import filecmp
+import orjson
+
+def test_main_S():
+    main.main("tests/output/t_outputS.json")
+    f = open("tests/records/t_recordsS.json", "r")
+    output = f.read()
+    print(output)
+    x = orjson.loads(output)
+    f = open("records.json", "r")
+    output = f.read()
+    y = orjson.loads(output)
+    assert(x == y)
+
+def test_main_M():
+    main.main("tests/output/t_outputM.json")
+    f = open("tests/records/t_recordsM.json", "r")
+    output = f.read()
+    x = orjson.loads(output)
+    f = open("records.json", "r")
+    output = f.read()
+    y = orjson.loads(output)
+    assert(x == y)
+
+def test_main_L():
+    main.main("tests/output/t_outputL.json")
+    f = open("tests/records/t_recordsL.json", "r")
+    output = f.read()
+    x = orjson.loads(output)
+    f = open("records.json", "r")
+    output = f.read()
+    y = orjson.loads(output)
+    assert(x == y)
+
+def test_main_XL():
+    main.main("tests/output/t_outputXL.json")
+    f = open("tests/records/t_recordsXL.json", "r")
+    output = f.read()
+    x = orjson.loads(output)
+    f = open("records.json", "r")
+    output = f.read()
+    y = orjson.loads(output)
+    assert(x == y)
